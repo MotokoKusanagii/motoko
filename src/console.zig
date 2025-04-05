48b94d00337618bb161a6ee0d5de30bb40812333
Meiko <meikoprivat@icloud.com>
2025-04-05 19:41:36 +0200

    Basic console like drawing

---
+++ src/console.zig
@@ -0,0 +1,68 @@
+const std = @import("std");
+const c = @import("c.zig");
+const sdl = c.sdl;
+
+const char_size = c.SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE;
+
+pub const Console = struct {
+    allocator: std.mem.Allocator,
+
+    // SDL stuff
+    renderer: *c.SDL_Renderer,
+
+    // Console stuff
+    x: f32,
+    y: f32,
+    scale: f32,
+    padding: f32,
+    entries: std.ArrayList([:0]const u8),
+
+    pub fn init(
+        allocator: std.mem.Allocator,
+        renderer: *c.SDL_Renderer,
+        x: f32,
+        y: f32,
+        scale: f32,
+        padding: f32,
+    ) Console {
+        return .{
+            .allocator = allocator,
+            .renderer = renderer,
+            .x = x,
+            .y = y,
+            .scale = scale,
+            .padding = padding,
+            .entries = std.ArrayList([:0]const u8).init(allocator),
+        };
+    }
+
+    pub fn deinit(self: *Console) void {
+        self.entries.deinit();
+    }
+
+    pub fn draw(self: Console) !void {
+        var r: u8 = 0;
+        var g: u8 = 0;
+        var b: u8 = 0;
+        var a: u8 = 0;
+        var scale_x: f32 = 0;
+        var scale_y: f32 = 0;
+        try sdl(c.SDL_GetRenderDrawColor(self.renderer, &r, &g, &b, &a));
+        try sdl(c.SDL_GetRenderScale(self.renderer, &scale_x, &scale_y));
+        try sdl(c.SDL_SetRenderDrawColor(self.renderer, 255, 255, 255, 255));
+        try sdl(c.SDL_SetRenderScale(self.renderer, self.scale, self.scale));
+        for (self.entries.items, 0..) |item, i| {
+            try sdl(c.SDL_RenderDebugText(self.renderer, self.x, self.y + calcY(i, self.padding), item));
+        }
+        try sdl(c.SDL_SetRenderDrawColor(self.renderer, r, g, b, a));
+        try sdl(c.SDL_SetRenderScale(self.renderer, scale_x, scale_y));
+    }
+
+    pub fn addEntry(self: *Console, text: [:0]const u8) !void {
+        try self.entries.append(text);
+    }
+
+    fn calcY(index: usize, padding: f32) f32 {
+        return (char_size + padding) * @as(f32, @floatFromInt(index));
+    }
+};
--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,58 @@
 const std = @import("std");
 const gl = @import("gl");
 const c = @import("c.zig");
+const Console = @import("console.zig").Console;
 const sdl = c.sdl;
+
+const text_scale = 2.5;
+const char_size = c.SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE;
 
 pub fn main() !void {
     c.SDL_SetMainReady();
+
+    try sdl(c.SDL_Init(c.SDL_INIT_VIDEO));
+
+    const window_flags = c.SDL_WINDOW_RESIZABLE;
+    const window: *c.SDL_Window = try sdl(c.SDL_CreateWindow("Motoko", 800, 400, window_flags));
+    defer c.SDL_DestroyWindow(window);
+    const renderer: *c.SDL_Renderer = try sdl(c.SDL_CreateRenderer(window, null));
+    defer c.SDL_DestroyRenderer(renderer);
+
+    var da = std.heap.DebugAllocator(.{}).init;
+    const allocator = da.allocator();
+
+    var console = Console.init(allocator, renderer, 0, 0, 2, 5);
+    defer console.deinit();
+
+    main_loop: while (true) {
+        var event: c.SDL_Event = undefined;
+        while (c.SDL_PollEvent(&event)) {
+            switch (event.type) {
+                c.SDL_EVENT_QUIT => {
+                    break :main_loop;
+                },
+                c.SDL_EVENT_KEY_DOWN => {
+                    switch (event.key.key) {
+                        c.SDLK_A => {
+                            try console.addEntry("this is an entry");
+                        },
+                        c.SDLK_D => {
+                            try console.addEntry("another entry");
+                        },
+                        else => {},
+                    }
+                },
+                else => {},
+            }
+        }
+
+        try sdl(c.SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255));
+        try sdl(c.SDL_RenderClear(renderer));
+
+        // Text
+        try console.draw();
+
+        try sdl(c.SDL_SetRenderScale(renderer, 1, 1));
+        try sdl(c.SDL_RenderPresent(renderer));
+    }
 }
