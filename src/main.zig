55a6f570180c3a715481f49c083f9ae770fdede0
Meiko <meikoprivat@icloud.com>
2025-04-08 00:08:55 +0200

    Integrate cpu.zig with interface system. Remove some stuff in main.zig

--- src/main.zig
+++ src/main.zig
@@ -4,7 +4,7 @@ const c = @import("c.zig");
 const sdl = c.sdl;
 
 const Console = @import("ui.zig").Console;
-const Nes = @import("nes.zig").Nes;
+const Cpu = @import("nes/cpu.zig").Cpu;
 
 const text_scale = 2.5;
 const char_size = c.SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE;
@@ -32,9 +32,6 @@ pub fn main() !void {
     defer console.deinit();
     try console.info("Console initialized.");
 
-    var nes: Nes = undefined;
-    nes.powerOn();
-
     main_loop: while (true) {
         var event: c.SDL_Event = undefined;
         while (c.SDL_PollEvent(&event)) {
@@ -49,40 +46,6 @@ pub fn main() !void {
                         },
                         c.SDLK_DOWN => {
                             console.scale -= 1;
-                        },
-                        // TODO: Create ui/list.zig and draw it there without
-                        // any input needed
-                        c.SDLK_C => {
-                            var buffer: [1024]u8 = undefined;
-                            var fmt: [:0]const u8 = undefined;
-                            fmt = try std.fmt.bufPrintZ(
-                                &buffer,
-                                "A: {X:0>2}, X: {X:0>2}, Y: {X:0>2}",
-                                .{ nes.cpu.x, nes.cpu.x, nes.cpu.y },
-                            );
-                            try console.info(fmt);
-
-                            fmt = try std.fmt.bufPrintZ(
-                                &buffer,
-                                "Stack Pointer: {X:0>2}",
-                                .{nes.cpu.sp},
-                            );
-                            try console.info(fmt);
-
-                            fmt = try std.fmt.bufPrintZ(
-                                &buffer,
-                                "Program Counter: {X:0>4}",
-                                .{nes.cpu.pc},
-                            );
-                            try console.info(fmt);
-
-                            fmt = try std.fmt.bufPrintZ(
-                                &buffer,
-                                "Status: 0b{b:0>8}",
-                                .{nes.cpu.status.data},
-                            );
-                            try console.info(fmt);
-                            try console.info("CPU state");
                         },
                         else => {},
                     }
@@ -100,4 +63,8 @@ pub fn main() !void {
         try sdl(c.SDL_SetRenderScale(renderer, 1, 1));
         try sdl(c.SDL_RenderPresent(renderer));
     }
+}
+
+test {
+    _ = @import("nes/cpu.zig");
 }
--- src/nes/cpu.zig
+++ src/nes/cpu.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const contracts = @import("../contracts.zig");
 
 pub const Status = struct {
     data: u8,
@@ -26,38 +27,85 @@ pub const Status = struct {
     }
 };
 
-pub const Cpu = struct {
-    // Register
-    status: Status,
-    pc: u16,
-    sp: u8,
-    a: u8,
-    x: u8,
-    y: u8,
-
-    pub const init = Cpu{
-        .status = .init,
-        .pc = 0x0000,
-        .sp = 0x00,
-        .a = 0x00,
-        .x = 0x00,
-        .y = 0x00,
-    };
-
-    pub fn powerOn(self: *Cpu) void {
-        self.a = 0;
-        self.x = 0;
-        self.y = 0;
-        self.pc = 0xFFFC;
-        self.sp = 0xFD;
-
-        self.status = .init;
-        self.status.set(.i, true);
+const BusInterface = contracts.Interface(struct {
+    pub fn read(_: @This(), addr: u16) u8 {
+        _ = addr;
     }
+    pub fn write(_: @This(), addr: u16, value: u8) void {
+        _ = addr;
+        _ = value;
+    }
+});
 
-    pub fn reset(self: *Cpu) void {
-        self.pc = 0xFFFC;
-        self.sp = 0xFD;
-        self.status.set(.i, true);
+pub fn Cpu(Bus: type) type {
+    BusInterface.validate(Bus);
+
+    return struct {
+        const Self = @This();
+
+        // Register
+        status: Status,
+        pc: u16,
+        sp: u8,
+        a: u8,
+        x: u8,
+        y: u8,
+
+        // Bus
+        bus: ?*Bus,
+
+        pub const init = Self{
+            .status = .init,
+            .pc = 0x0000,
+            .sp = 0x00,
+            .a = 0x00,
+            .x = 0x00,
+            .y = 0x00,
+            .bus = null,
+        };
+
+        pub fn powerOn(self: *Self) void {
+            self.a = 0;
+            self.x = 0;
+            self.y = 0;
+            self.pc = 0xFFFC;
+            self.sp = 0xFD;
+
+            self.status = .init;
+            self.status.set(.i, true);
+        }
+
+        pub fn reset(self: *Self) void {
+            self.pc = 0xFFFC;
+            self.sp = 0xFD;
+            self.status.set(.i, true);
+        }
+
+        pub fn clock(self: Self) void {
+            _ = self;
+            // Check if self.cycles is zero
+            // Read instruction at self.pc
+            // address mode
+            // instruction
+            // check extra cycle
+
+        }
+    };
+}
+
+const TestBus = struct {
+    pub fn read(self: TestBus, addr: u16) u8 {
+        _ = self;
+        _ = addr;
+    }
+    pub fn write(self: TestBus, addr: u16, value: u8) void {
+        _ = self;
+        _ = addr;
+        _ = value;
     }
 };
+
+test "cpu init" {
+    const cpu = Cpu(TestBus).init;
+    _ = cpu;
+}
