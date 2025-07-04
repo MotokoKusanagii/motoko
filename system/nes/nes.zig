3cf76f89437200c8d3cab963dcd566ca8b0f12c2
Meiko <meikoprivat@icloud.com>
2025-07-04 20:23:29 +0200

    add nes structure

--- system/nes/nes.zig
+++ system/nes/nes.zig
@@ -1,8 +1,32 @@
-const mos = @import("mos_technology_6502.zig");
-const Chip = mos.Chip;
+const std = @import("std");
+const Mos6502 = @import("mos_technology_6502");
 
-pub const System = struct {
-    pub fn init() System {
-        return .{};
-    }
+const Nes = @This();
+
+alloc: std.mem.Allocator,
+cpu: Mos6502 = undefined,
+
+pub fn init(self: *Nes, alloc: std.mem.Allocator) void {
+    self.alloc = alloc;
+    self.cpu = .init(self.bus());
+}
+
+pub fn startup(self: *Nes) void {
+    self.cpu.reset();
+}
+
+fn read(_: *anyopaque, _: u16) u8 {}
+
+fn write(_: *anyopaque, _: u16, _: u8) void {}
+
+const bus_vtable: Mos6502.Bus.VTable = .{
+    .read = read,
+    .write = write,
 };
+
+fn bus(self: *Nes) Mos6502.Bus {
+    return .{
+        .ptr = self,
+        .vtable = bus_vtable,
+    };
+}
